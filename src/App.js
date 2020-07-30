26984069a29077bdcf42442eb830a58b40236ea0
Isaiah Murray <murray.isaiah13@gmail.com>
2020-07-30 14:06:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Navbar from './components/Navbar/Navbar';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar/> */}
+      <h1 className="shopTitle"
+      style={{textAlign: 'center', marginTop: '-3em'}}>Shop Records!</h1>
     </div>
   );
 }
